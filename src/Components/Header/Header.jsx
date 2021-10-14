0d46c7e09bc6416723d200cc4da6f1807b2d35bb
cusen11 <you@example.com>
2021-10-14 10:54:15 +0700

    header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,23 +1,24 @@
 import { Grid, Typography } from '@material-ui/core';
 import React from 'react';
 import Menu from '../Menu/Menu';
-import './Header.css';
-import ButtonLink from '../Button/ButtonLink'; 
+import './Header.css'; 
 import ButtonPopup from '../Button/ButtonPopup';
 import IconButton from '../../Assets/Images/arrow.png';
 function Header() {
     return (
         <div className="header">
             <Menu/>
-            <Grid className="header_hero-text">
-                <Typography variant="h1">
-                    <span className="text-1">Chăm sóc</span>
-                    <span className="text-2">Làn da của bạn</span>
-                    <span className="text-3">Là niềm hạnh phúc của </span>
-                    <span className="text-4">Huyền Phi</span>
-                </Typography>
-                <ButtonLink text="NHẬN ƯU ĐÃI" link="link" position="left down" icon={IconButton}/>
-                <ButtonPopup text="NHẬN ƯU ĐÃI" link="link" position="right up" icon={IconButton}/>
+            <Grid container className="header_hero" justifyContent="flex-start" alignItems="center">
+                <Grid item className="header_hero-text">
+                    <Typography variant="h1">
+                        <span className="text-1">Chăm sóc</span>
+                        <span className="text-2">Làn da của bạn</span>
+                        <span className="text-3">Là niềm hạnh phúc của </span>
+                        <span className="text-4">Huyền Phi</span>
+                    </Typography>
+                    <Typography variant="h4">Liên hệ ngay với Huyền Phi<br/> để được nhận ưu đãi</Typography> 
+                    <ButtonPopup text="NHẬN ƯU ĐÃI" link="link" position="right" icon={IconButton}/>
+                </Grid>
             </Grid>
         </div>
     );
